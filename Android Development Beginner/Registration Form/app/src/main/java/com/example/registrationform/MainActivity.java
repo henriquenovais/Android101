06449046c90f61b9af078bd38cde53c998b535bf
henrique <henriquennoliveira@gmail.com>
2020-10-12 13:55:49 -0300

    implement sign up screen

--- Android Development Beginner/Registration Form/app/src/main/java/com/example/registrationform/MainActivity.java
+++ Android Development Beginner/Registration Form/app/src/main/java/com/example/registrationform/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.registrationform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSignUpClick (View view) {
+        // Declaring objects to extract information from Views:
+        EditText editTextUsername = findViewById(R.id.editTextUsername);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPassword= findViewById(R.id.editTextPassword);
+        TextView textViewUsername = findViewById(R.id.textViewUsername);
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+        TextView textViewPassword = findViewById(R.id.textViewPassword);
+
+        // Change View information according to the information input by the user.
+        textViewUsername.setText("User: " + editTextUsername.getText().toString());
+        textViewEmail.setText("E-mail: " + editTextEmail.getText().toString());
+        textViewPassword.setText("Password: " + editTextPassword.getText().toString());
+
+
     }
 }
