4953130146d9690e928df474c19e2c327b0e3091
henrique <henriquennoliveira@gmail.com>
2020-10-11 23:26:46 -0300

    add text update app

--- Android Development Beginner/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Android Development Beginner/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView txtMessage = findViewById(R.id.txtMessage);
+        TextView txtInput = findViewById(R.id.txtInput);
+    }
+
+
+    public void onBtnClick(View view) {
+
+        TextView txtMessage = findViewById(R.id.txtMessage);
+        TextView txtInput = findViewById(R.id.txtInput);
+
+        String txt = (String) txtInput.getText();
+        txtMessage.setText(txt);
     }
 }
