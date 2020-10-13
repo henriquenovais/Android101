a4755a875a3900146b916934b7425db326e5f379
henrique <henriquennoliveira@gmail.com>
2020-10-13 18:35:29 -0300

    add java revision challenge

---
+++ Android Development Beginner/JavaRevision/src/org/maincode/gamechallenge/Main.java
@@ -0,0 +1,15 @@
+package org.maincode.gamechallenge;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        GAME CHALLENGE
+        Ask the user to guess a random number
+        If the user fails five times in a row display a game over screen
+        * */
+
+        System.out.println("Hello World");
+    }
+}
