a981f14042722df7ab070531256628dc12ea68c7
henrique <henriquennoliveira@gmail.com>
2020-10-16 23:27:57 -0300

    add oop concepts challenge java

--- Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Database.java
+++ Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Database.java
@@ -1,4 +1,16 @@
 package org.java.oopconceptschallenge;
 
+import java.util.ArrayList;
+
 public class Database {
+
+    public static class Contacts {
+
+        private static ArrayList<String> firstNames = new ArrayList<>();
+        private static ArrayList<String> lastNames = new ArrayList<>();
+        private static ArrayList<int> phoneNumbers = new ArrayList<>();
+
+        public Contacts() {
+        }
+    }
 }
