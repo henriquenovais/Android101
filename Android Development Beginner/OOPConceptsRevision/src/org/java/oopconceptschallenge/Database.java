590dc994bba190af8d184c8c3019c98220b14ad2
henrique <henriquennoliveira@gmail.com>
2020-10-17 22:42:19 -0300

    implement oop concepts challenge java

--- Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Database.java
+++ Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Database.java
@@ -4,13 +4,58 @@ import java.util.ArrayList;
 
 public class Database {
 
-    public static class Contacts {
+    public static class ContactList {
 
-        private static ArrayList<String> firstNames = new ArrayList<>();
-        private static ArrayList<String> lastNames = new ArrayList<>();
-        private static ArrayList<int> phoneNumbers = new ArrayList<>();
+        private static ArrayList<Contact> contacts = new ArrayList<>();
 
-        public Contacts() {
+        public static void addContact(Contact contact) {
+            contacts.add(contact);
         }
     }
+
+    public static class Contact {
+
+        private String firstName;
+        private String lastName;
+        private int phoneNumber;
+
+        public Contact(String firstName, String lastName, int phoneNumber) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public int getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void printContact() {
+            System.out.println("First name: " + this.firstName);
+            System.out.println("Last name: " + this.lastName);
+            System.out.println("Phone number: " + this.phoneNumber);
+
+        }
+    }
+
+
 }
---
+++ Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Main.java
@@ -0,0 +1,10 @@
+package org.java.oopconceptschallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Database.Contact Henrique = new Database.Contact("Henrique", "Oliveira", 99999999);
+
+        Henrique.printContact();
+
+    }
+}
