b763a1f738034aa789c4a1569114049e212bc954
henrique <henriquennoliveira@gmail.com>
2020-10-20 18:01:23 -0300

    implement oop concepts challenge

--- Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Main.java
+++ Android Development Beginner/OOPConceptsRevision/src/org/java/oopconceptschallenge/Main.java
@@ -22,10 +22,10 @@ public class Main {
 
             switch (menu) {
                 case 1:
-                    manageContactsMenu();
+                    menuManageContacts();
                     break;
                 case 2:
-                    messagesMenu();
+                    menuMessages();
                     break;
                 case 3:
                     System.out.println("Exiting application");
@@ -38,11 +38,12 @@ public class Main {
         }
     }
 
-    public static void manageContactsMenu() {
+    public static void menuManageContacts() {
 
         Scanner Sc = new Scanner(System.in);
         int menu = 0;
         boolean loopMenu = true;
+        while(loopMenu){
 
         System.out.println("Welcome to the contact list and messaging app !");
         System.out.println("1. Show all contacts");
@@ -58,23 +59,74 @@ public class Main {
                 showAllContacts();
                 break;
             case 2:
-                messagesMenu();
+                menuAddContact();
                 break;
             case 3:
-                System.out.println("Exiting application");
+                menuSearchForContact();
+                break;
+            case 4:
+                menuDeleteContact();
+                break;
+            case 5:
+                System.out.println("Returning to previous menu");
                 loopMenu = false;
                 break;
             default:
                 System.out.println("Invalid input. Please choose a valid number.");
                 break;
+            }
         }
     }
-    }
 
-    public static void messagesMenu(){
+    public static void menuMessages() {
 
+        Scanner Sc = new Scanner(System.in);
+        int menu = 0;
+        boolean loopMenu = true;
+
+        while(loopMenu){
+
+            System.out.println("Welcome to the contact list and messaging app !");
+            System.out.println("1. Show all messages");
+            System.out.println("2. Send a new message");
+            System.out.println("3. Go back to the previous menu");
+            System.out.print("What is your input? ");
+            menu = Sc.nextInt();
+
+            switch (menu) {
+                case 1:
+                    showAllMessages();
+                    break;
+                case 2:
+                    sendMessage();
+                    break;
+                case 3:
+                    System.out.println("Returning to previous menu");
+                    loopMenu = false;
+                    break;
+                default:
+                    System.out.println("Invalid input. Please choose a valid number.");
+                    break;
+            }
+        }
     }
 
     public static void showAllContacts() {
     }
+
+    public static void menuAddContact() {
+    }
+
+    public static void menuSearchForContact() {
+    }
+
+    public static void menuDeleteContact() {
+    }
+
+    public static void showAllMessages() {
+    }
+
+    public static void sendMessage() {
+    }
+
 }
