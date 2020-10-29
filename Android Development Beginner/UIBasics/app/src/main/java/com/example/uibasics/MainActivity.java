401e2687579eafc306f6490e190e967ab81c9f69
henrique <henriquennoliveira@gmail.com>
2020-10-29 20:25:06 -0300

    finish UI basics exercise

--- Android Development Beginner/UIBasics/app/src/main/java/com/example/uibasics/MainActivity.java
+++ Android Development Beginner/UIBasics/app/src/main/java/com/example/uibasics/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onHelloButtonClicked(View view){
+        TextView TextViewWelcome = findViewById(R.id.TextViewWelcome);
+        TextViewWelcome.setText("Hello Again");
     }
 }
